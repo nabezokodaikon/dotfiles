a31b2a5332782fed1b009727fc9a29c38e6a5c48
main <main@example.com>
2015-05-28 15:25:51 +0900

    インストールスクリプトを作成。

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 各種設定ファイルのシンボリックリンクを作成します。
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.ctags $HOME/.ctags
+ln -s $HOME/dotfiles/.sbtconfig $HOME/.sbtconfig
+
+exit 0
