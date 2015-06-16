c1efe0edaac641b05f241c71e84922d090f699a8
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-06-16 22:35:21 +0900

    git の設定を行うスクリプトを作成。

---
+++ git-config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "nabezokodaikon"
+git config --global user.email "nabezoko.daikon@gmail.com"
+git config --global color.ui.auto
+git config --global core.editor vim
