cacc1abfba2c7584a2923c9c0cef73e8f68a4332
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-07-18 15:58:26 +0900

    git を https 経由で使うときのパスワードを保存する
    参考
    http://qiita.com/usamik26/items/c655abcaeee02ea59695

--- git-config.sh
+++ git-config.sh
@@ -4,3 +4,4 @@ git config --global user.name "nabezokodaikon"
 git config --global user.email "nabezoko.daikon@gmail.com"
 git config --global color.ui.auto
 git config --global core.editor vim
+git config --global credential.helper cache --timeout=36000
