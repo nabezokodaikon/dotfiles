acaccaacb119e77b0537f25eb559420f03cd74e9
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-04-06 22:36:24 +0900

    add tigの設定を作成

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.ctags $HOME/.ctags
 ln -s $HOME/dotfiles/.sbtconfig $HOME/.sbtconfig
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/.tigrc $HOME/.tigrc
 
 exit 0
