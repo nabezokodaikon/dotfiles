3989d0d493c850e796bea8c18cb6a2d44400b2b0
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-05-18 22:33:34 +0900

    clean nvimの設定ファイルを.configディレクトリへ移動
    $HOME/.configディレクトリに設定ファイルを配置するアプリケーションが他にも存在し得るため。
    install.shのスクリプトを修正。

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.ctags $HOME/.ctags
 ln -s $HOME/dotfiles/.sbtconfig $HOME/.sbtconfig
-ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/dotfiles/.tigrc $HOME/.tigrc
+ln -s $HOME/dotfiles/.config/nvim/ $HOME/.config/nvim
 
 exit 0
