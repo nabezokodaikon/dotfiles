5876c353f69923c0bc6e87c4930a0f1b3ac13c8f
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-09-19 11:03:13 +0900

    add homebrew setting
    HOMEBREW_CASK_OPTSを設定した。

--- zsh/osx.sh
+++ zsh/osx.sh
@@ -12,6 +12,7 @@ export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=.:$PATH
 export PATH=$HOME/bin:$PATH
+export HOMEBREW_CASK_OPTS="--caskroom=/opt/homebrew-cask/Caskroom"
 
 alias vim='env LANG=ja_JP.UTF-8 /usr/local/bin/vim "$@"'
 alias ctags='/usr/local/bin/ctags'
