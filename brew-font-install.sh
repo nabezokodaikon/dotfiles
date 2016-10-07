553db3f6daef38f60c253c856218220e47be6fb7
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-10-07 23:24:38 +0900

    Clean 不要ファイルを削除

--- brew-font-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-brew tap caskroom/fonts
-
-fonts=(
-    font-m-plus
-)
-
-echo "installing fonts..."
-brew cask install ${fonts[@]}
--- neobundle-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Vim のプラグイン管理ツール NeoBundle をインストールします。 
-git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
-
-# Vim のプラグインをインストールします。
-vim -u $HOME/dotfiles/.vimrc.init -c NeoBundleInstall! -c q
-
-exit 0
