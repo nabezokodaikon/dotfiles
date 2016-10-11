6af2ab1855bee0280970b6fc6db97de83f188d52
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-10-11 14:11:31 +0900

    Clean OSXのZshの余分な空白を削除

--- zsh/osx.sh
+++ zsh/osx.sh
@@ -26,19 +26,19 @@ if [ -d "$(brew --cellar)/nvm" ]; then
     export NVM_DIR="$HOME/.nvm"
     nvm() {
         unset -f nvm
-        [ -s "$(brew --prefix nvm)/nvm.sh" ] && source "$(brew --prefix nvm)/nvm.sh" 
+        [ -s "$(brew --prefix nvm)/nvm.sh" ] && source "$(brew --prefix nvm)/nvm.sh"
         nvm "$@"
     }
 
     node() {
         unset -f node
-        [ -s "$(brew --prefix nvm)/nvm.sh" ] && source "$(brew --prefix nvm)/nvm.sh" 
+        [ -s "$(brew --prefix nvm)/nvm.sh" ] && source "$(brew --prefix nvm)/nvm.sh"
         node "$@"
     }
 
     npm() {
         unset -f npm
-        [ -s "$(brew --prefix nvm)/nvm.sh" ] && source "$(brew --prefix nvm)/nvm.sh" 
+        [ -s "$(brew --prefix nvm)/nvm.sh" ] && source "$(brew --prefix nvm)/nvm.sh"
         npm "$@"
     }
 fi
