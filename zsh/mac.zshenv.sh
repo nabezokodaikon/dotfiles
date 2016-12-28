b69037e6b2c3624a65fe419de0ad4d819466264f
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-12-28 10:28:37 +0900

    Remove PYTHONPATH
    It is not necessary to specify it.

--- zsh/mac.zshenv.sh
+++ zsh/mac.zshenv.sh
@@ -12,11 +12,6 @@ export HOMEBREW_CASK_OPTS="--caskroom=/opt/homebrew-cask/Caskroom"
 export JAVA_HOME=/Library/Java/Home
 
 
-# Python
-#--------------------------------
-export PYTHONPATH=/usr/local/lib/python3.5/site-packages/
-
-
 # nvm
 #--------------------------------
 export NVM_DIR="$HOME/.nvm"
