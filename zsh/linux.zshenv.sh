11e71855eabb2bbe0f85320c718b2aefeccdfdfa
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-07-05 08:28:38 +0900

    Add Neovim log file path

--- zsh/linux.zshenv.sh
+++ zsh/linux.zshenv.sh
@@ -10,3 +10,9 @@ export JAVA_HOME=$(readlink -f `which javac 2>/dev/null` | sed "s:/bin/javac::")
 # Scala
 #--------------------------------
 export SCALA_HOME=$(readlink -f `which scalac 2>/dev/null` | sed "s:/bin/scalac::") 
+
+
+# Python
+#--------------------------------
+export NVIM_PYTHON_LOG_FILE=/tmp/log
+export NVIM_PYTHON_LOG_LEVEL=DEBUG
