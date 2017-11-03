10a49142a190073be52288597bb094e96d0997ae
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-11-04 06:03:15 +0900

    Remove unneed settings

--- mac/karabiner/export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Karabinerの設定を出力します。
-
-cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
-
-$cli export > import.sh
--- mac/karabiner/reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Karabinerの設定をリセットします。
-# インポートの前に実行してください。
-
-profile_name=nabezokodaikon
-cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
-
-$cli be_careful_to_use__clear_all_values_by_name $profile_name
