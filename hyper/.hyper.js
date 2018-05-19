6c75489846299c96a1a53033bcfafbed81e78c98
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-19 23:04:59 +0900

    Add Hyper color theme

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -131,7 +131,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyper-atom-dark-transparent'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
