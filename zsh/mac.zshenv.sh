e19091089c8eb6f02c5e71c16bd992e7c7956502
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-05-24 22:00:27 +0900

    Add JAVA_HOME for Mac

--- zsh/mac.zshenv.sh
+++ zsh/mac.zshenv.sh
@@ -12,3 +12,9 @@ export PATH=$PATH:$HOME/.cargo/bin
 #--------------------------------
 export NVIM_PYTHON_LOG_FILE=/tmp/log
 export NVIM_PYTHON_LOG_LEVEL=DEBUG
+
+
+# Java
+# $ brew cask install caskroom/versions/adoptopenjdk8
+#--------------------------------
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
