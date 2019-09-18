c68b80b6564f582b698a9d85e97873c06023f97d
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-09-18 20:32:45 +0900

    Add ginit.vim symlink command

--- script/install-windows.bat
+++ script/install-windows.bat
@@ -5,4 +5,5 @@ rmdir %USERPROFILE%\.config
 REM Install
 mkdir %LOCALAPPDATA%\nvim
 mklink %LOCALAPPDATA%\nvim\init.vim %USERPROFILE%\dotfiles\.config\nvim\init.vim
+mklink %LOCALAPPDATA%\nvim\ginit.vim %USERPROFILE%\dotfiles\.config\nvim\ginit.vim
 mklink /D %USERPROFILE%\.config %USERPROFILE%\dotfiles\.config
