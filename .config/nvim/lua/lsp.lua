f23b69e047a45685705c27b14a4094d0098e91e1
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-13 01:20:25 +0900

    Clean lsp of lua setting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,34 +1,27 @@
-local M = {}
+vim.cmd('packadd nvim-lsp')
+require'nvim_lsp'.rls.setup{}
+require'nvim_lsp'.rust_analyzer.setup{}
+require'nvim_lsp'.metals.setup{}
 
-function M.makeScratch()
-    local nvim_lsp = require'nvim_lsp'
-    local configs = require'nvim_lsp/skeleton'
-
-    -- Rust
-    require'nvim_lsp'.rls.setup{}
-    -- nvim_lsp.rls.setup {
-        -- settings = {
-            -- rust = {
-                -- clippy_preference = 'off';
-                -- show_warnings = false;
-                -- show_hover_context = false;
-            -- }
+-------------------------
+-- Other setting example
+-------------------------
+-- nvim_lsp.rls.setup {
+    -- settings = {
+        -- rust = {
+            -- clippy_preference = 'off';
+            -- show_warnings = false;
+            -- show_hover_context = false;
         -- }
     -- }
-    -- rust_analyzer
-    require'nvim_lsp'.rust_analyzer.setup{}
+-- }
 
-    -- Scala
-    require'nvim_lsp'.metals.setup{}
-    -- if not nvim_lsp.scala_lsp then
-        -- configs.scala_lsp = {
-            -- default_config = {
-                -- cmd = {'metals-vim'};
-                -- filetypes = {'scala', 'sbt'};
-                -- root_dir = nvim_lsp.util.root_pattern('build.sbt');
-        -- };
-      -- }
-    -- end
-end
-
-return M
+-- if not nvim_lsp.scala_lsp then
+    -- configs.scala_lsp = {
+        -- default_config = {
+            -- cmd = {'metals-vim'};
+            -- filetypes = {'scala', 'sbt'};
+            -- root_dir = nvim_lsp.util.root_pattern('build.sbt');
+    -- };
+  -- }
+-- end
