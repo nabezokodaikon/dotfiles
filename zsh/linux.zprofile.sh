8e9ef2d3739bf99c9373dd4076f001a0f012f091
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-29 22:28:47 +0900

    Fix linuxbrew install setting

--- zsh/linux.zprofile.sh
+++ zsh/linux.zprofile.sh
@@ -4,6 +4,4 @@
 
 # Homebrew-on-Linux
 #--------------------------------
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+eval \$($(brew --prefix)/bin/brew shellenv)
