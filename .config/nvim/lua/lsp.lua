b1fec81669817da9dfda8d51008879569dd9de64
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-05-13 07:33:06 +0900

    Modify reference rust-analyzer file name

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -10,5 +10,5 @@ require'nvim_lsp'.rls.setup{}
 -- require'nvim_lsp'.rust_analyzer.setup{}
 -- local nvim_lsp = require'nvim_lsp'
 -- nvim_lsp.rust_analyzer.setup {
-    -- cmd = { '~/.local/bin/rust-analyzer-mac' }
+    -- cmd = { '~/.local/bin/rust-analyzer' }
 -- }
