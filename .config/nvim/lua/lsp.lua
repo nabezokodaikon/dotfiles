6b1f0556de6454fdd704cdb937ee18e1e35ab8d5
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-05-13 07:26:09 +0900

    Modify LSP setting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,27 +1,14 @@
-vim.cmd('packadd nvim-lsp')
-require'nvim_lsp'.rls.setup{}
-require'nvim_lsp'.rust_analyzer.setup{}
+--------------------------------
+-- LSP configurations
+--------------------------------
 require'nvim_lsp'.metals.setup{}
 
--------------------------
--- Other setting example
--------------------------
--- nvim_lsp.rls.setup {
-    -- settings = {
-        -- rust = {
-            -- clippy_preference = 'off';
-            -- show_warnings = false;
-            -- show_hover_context = false;
-        -- }
-    -- }
--- }
 
--- if not nvim_lsp.scala_lsp then
-    -- configs.scala_lsp = {
-        -- default_config = {
-            -- cmd = {'metals-vim'};
-            -- filetypes = {'scala', 'sbt'};
-            -- root_dir = nvim_lsp.util.root_pattern('build.sbt');
-    -- };
-  -- }
--- end
+require'nvim_lsp'.rls.setup{}
+
+
+-- require'nvim_lsp'.rust_analyzer.setup{}
+-- local nvim_lsp = require'nvim_lsp'
+-- nvim_lsp.rust_analyzer.setup {
+    -- cmd = { '~/.local/bin/rust-analyzer-mac' }
+-- }
