a37a567ed9e3b1117d39ce7036d629967567aa2c
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-05-17 13:25:57 +0900

    Modify Rust LSP

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -4,10 +4,7 @@
 require'nvim_lsp'.metals.setup{}
 
 
-require'nvim_lsp'.rls.setup{}
-
-
--- require'nvim_lsp'.rust_analyzer.setup{}
+require'nvim_lsp'.rust_analyzer.setup{}
 
 
 require'nvim_lsp'.pyls.setup{}
