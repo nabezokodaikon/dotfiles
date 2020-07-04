dd725f2652bf5e2b52ac760598fa690b39a15b8a
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-07-05 03:47:24 +0900

    Add tig setting for windows

--- script/install-windows.bat
+++ script/install-windows.bat
@@ -1,9 +1,11 @@
 REM Uninstall
 rmdir /s /q %LOCALAPPDATA%\nvim
 rmdir %USERPROFILE%\.config
+del .tigrc
 
 REM Install
 mkdir %LOCALAPPDATA%\nvim
 mklink %LOCALAPPDATA%\nvim\init.vim %USERPROFILE%\dotfiles\.config\nvim\init.vim
 mklink %LOCALAPPDATA%\nvim\ginit.vim %USERPROFILE%\dotfiles\.config\nvim\ginit.vim
 mklink /D %USERPROFILE%\.config %USERPROFILE%\dotfiles\.config
+mklink %USERPROFILE%\.tigrc %USERPROFILE%\dotfiles\tig\.tigrc
