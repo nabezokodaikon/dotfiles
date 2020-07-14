61308490eff0508405b71edfbfdb70376e5e0331
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-07-15 02:21:38 +0900

    Add tig LANG settings

--- zsh/linux.zshrc.sh
+++ zsh/linux.zshrc.sh
@@ -23,6 +23,7 @@ alias ls="ls --color=auto"
 alias vim="env LANG=en_US.UTF-8 vim \"\$@\""
 alias nvim="env LANG=en_US.UTF-8 nvim \"\$@\""
 alias tmux="env LANG=en_US.UTF-8 tmux -2u \"\$@\""
+alias tig="env LANG=en_US.UTF-8 tig \"\$@\""
 alias pip="LC_ALL=C pip"
 alias dl="docker ps -l -q"
 
