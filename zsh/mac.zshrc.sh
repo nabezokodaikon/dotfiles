ffc0fe11d0b33e00c8609c186c7cd608a2b699a2
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-09 15:29:41 +0900

    Add deno path

--- zsh/mac.zshrc.sh
+++ zsh/mac.zshrc.sh
@@ -10,7 +10,7 @@ export PATH=/usr/local/opt/node@10/bin:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
-
+export PATH=$HOME/.deno/bin:$PATH
 
 # Alias
 #--------------------------------
