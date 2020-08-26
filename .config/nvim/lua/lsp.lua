400809a5e97738888b46d250dd88d2966fa6927a
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-26 11:05:27 +0900

    Modify lsp settings

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,22 +1,46 @@
 --------------------------------
 -- LSP configurations
 --------------------------------
-local lsp = require "nvim_lsp"
+local lsp = require'nvim_lsp'
 -- local completion = require "completion"
 -- local diagnostic = require "diagnostic"
 
 function on_attach()
-    -- diagnostic.on_attach()
     -- completion.on_attach()
+    -- diagnostic.on_attach()
 end
+
+local capabilities = {
+    textDocument = {
+        completion = {
+            completionItem = {
+                snippetSupport = false
+            }
+        }
+    }
+};
+
 
 lsp.metals.setup{}
 
 
-lsp.rust_analyzer.setup{ on_attach = on_attach }
-
-
 lsp.pyls_ms.setup{}
+
+
+lsp.rust_analyzer.setup {
+    settings = {
+        ["rust-analyzer"] = {
+            completion = {
+                addCallArgumentSnippets = false,
+                addCallParenthesis = false,
+                postfix = {
+                    enable = false,
+                }
+            }
+        }
+    },
+    capabilities = capabilities,
+}
 
 
 lsp.tsserver.setup{}
