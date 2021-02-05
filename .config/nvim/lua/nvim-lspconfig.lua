2900b7c799955f4d89dcda16f243e66a86505977
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-02-06 00:53:03 +0900

    Clean lsp setting of lua

--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -3,7 +3,7 @@
 --------------------------------
 local lsp = require'lspconfig'
 local completion = require'completion'
-local util = require 'lspconfig/util'
+local util = require'lspconfig/util'
 
 local capabilities = {
     textDocument = {
@@ -14,6 +14,10 @@ local capabilities = {
         },
     },
 }
+
+local on_attach = function(client)
+    completion.on_attach(client)
+end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -26,124 +30,15 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 
 lsp.tsserver.setup {
-    on_attach = completion.on_attach,
-    cmd = {
-        "typescript-language-server", "--stdio"
-    },
-    filetypes = {
-        "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"
-    },
-    root_dir = util.root_pattern("package.json", "tsconfig.json", ".git"),
+    on_attach = on_attach,
+    capabilities = capabilities,
 }
 
+lsp.rust_analyzer.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
 
 -- lsp.metals.setup {}
 
-
 -- lsp.pyls_ms.setup {}
-
-
-lsp.rust_analyzer.setup {
-    settings = {
-        ["rust-analyzer"] = {
-            assist = {
-                importMergeBehaviour = "full",
-                importPrefix = "plain"
-            },
-            callInfo = true,
-            cargo = {
-                allFeatures = true,
-                autoreload = true,
-                features = {},
-                loadOutDirsFromCheck = nil,
-                noDefaultFeatures = nil,
-                target = vim.NIL,
-            },
-            cargoRunner = vim.NIL,
-            checkOnSave = {
-                allFeatures = vim.NIL,
-                allTargets = true,
-                command = "check",
-                enable = true,
-                extraArgs = {},
-                features = vim.NIL,
-                noDefaultFeatures = vim.NIl,
-                overrideCommand = vim.NIl,
-                target = vim.Nil,
-            },
-            completion = {
-                addCallArgumentSnippets = true,
-                addCallParenthesis = true,
-                postfix = {
-                    enable = true,
-                },
-            },
-            debug = {
-                engine = "auto",
-                engineSettings = vim.empty_dict(),
-                openDebugPane = false,
-                sourceFileMap = {["/rustc/<id>"] = "${env:USERPROFILE}/.rustup/toolchains/<toolchain-id>/lib/rustlib/src/rust"},
-            },
-            diagnostics = {
-                disabled = {},
-                enable = true,
-                enableExperimental = true,
-                warningsAsHint = {},
-            },
-            files = {
-                exclude = {},
-                watcher = "client",
-            },
-            hoverActions = {
-                debug = true,
-                enable = true,
-                gotoTypeDef = true,
-                implementations = true,
-                run = true,
-            },
-            inlayHints = {
-                chainingHints = true,
-                enable = true,
-                maxLength = 20,
-                parameterHints = true,
-                typeHints = true,
-            },
-            lens = {
-                debug = true,
-                enable = true,
-                implementations = true,
-                methodReferences = nil,
-                run = true,
-            },
-            linkedProjects = vim.NIl,
-            lruCapacity = vim.NIL,
-            noSysroot = nil,
-            notifications = {
-                cargoTomlNotFound = true,
-            },
-            procMacro = {
-                enable = true,
-            },
-            runnableEnv = vim.NIL,
-            runnables = {
-                cargoExtraArgs = {},
-                overrideCargo = vim.NIL,
-            },
-            rustcSource = vim.NIL,
-            rustfmt = {
-                extraArgs = {},
-                overrideCommand = vim.NIL,
-            },
-            serverPath = vim.NIL,
-            trace = {
-                extension = false,
-                server = "off",
-            },
-            updates = {
-                askBeforeDownload = true,
-                channel = "stable"
-            },
-        },
-    },
-    capabilities = capabilities,
-}
