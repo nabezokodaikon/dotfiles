11d18d1d2bc3290346a600608c1b79f9ead573c1
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-02-07 21:58:05 +0900

    Modify completion-nvim to disable

--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -2,7 +2,7 @@
 -- LSP configurations
 --------------------------------
 local lsp = require'lspconfig'
-local completion = require'completion'
+-- local completion = require'completion'
 local util = require'lspconfig/util'
 
 local capabilities = {
@@ -15,9 +15,9 @@ local capabilities = {
     },
 }
 
-local on_attach = function(client)
-    completion.on_attach(client)
-end
+-- local on_attach = function(client)
+    -- completion.on_attach(client)
+-- end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -30,12 +30,12 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 
 lsp.tsserver.setup {
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     capabilities = capabilities,
 }
 
 lsp.rust_analyzer.setup {
-    on_attach = on_attach,
+    -- on_attach = on_attach,
     capabilities = capabilities,
 }
 
