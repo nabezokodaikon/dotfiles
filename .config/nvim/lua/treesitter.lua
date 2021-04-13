86e588b0dbc0c1761ac0c2cd603ddfe13a64ff75
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-04-14 01:46:13 +0900

    Add treesitter lua file

---
+++ .config/nvim/lua/treesitter.lua
@@ -0,0 +1,9 @@
+local treesitter = require'nvim-treesitter.configs';
+
+treesitter.setup {
+    ensure_installed = "maintained",
+    highlight = {
+        enable = true,
+        disable = { "rust" },
+    },
+}
