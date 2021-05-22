d274973c03f42bb78e4a5ea3cfcfca54461a680a
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-05-22 21:16:10 +0900

    Modify rust_analyzer setting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -35,7 +35,20 @@ lsp.tsserver.setup {
 }
 
 lsp.rust_analyzer.setup {
-    -- on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            assist = {
+                importGranularity = "module",
+                importPrefix = "by_self",
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
+            },
+            procMacro = {
+                enable = true
+            },
+        },
+    },
 }
 
 -- lsp.metals.setup {}
