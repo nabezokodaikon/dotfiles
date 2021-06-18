8ca9f66fc0b7ff50ae08dc9399491bdd82b0eeda
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-06-19 06:41:15 +0900

    Add current direcotry history function on fzf

--- zsh/common.zshrc.sh
+++ zsh/common.zshrc.sh
@@ -41,20 +41,22 @@ bindkey "^n" history-beginning-search-forward-end
 bindkey "^p" history-beginning-search-backward-end
 bindkey "^n" history-beginning-search-forward-end
 
-# git の情報を表示
-autoload -U vcs_info
-setopt prompt_subst
-zstyle ":vcs_info:*" enable git
-zstyle ":vcs_info:git:*" check-for-changes true
-zstyle ":vcs_info:git:*" stagedstr '%F{yellow}!'
-zstyle ":vcs_info:git:*" unstagedstr '%F{red}+'
-zstyle ":vcs_info:*" formats '%F{green}%c%u[%b:%r]%f'
-zstyle ":vcs_info:*" actionformats '[%b:%r|%a]'
-precmd () { vcs_info }
-RPROMPT='${vcs_info_msg_0_}'
+# ノーマルモードに移行するキーを変更する。
+bindkey -M viins 'jj' vi-cmd-mode
+
+# 行頭、行末移動
+bindkey -M vicmd '^h' vi-digit-or-beginning-of-line
+bindkey -M vicmd '^l' vi-end-of-line
+
+# historyに保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks
+
+# ウィンドウ、ペイン間でコマンド履歴を共有する。
+setopt share_history
+
 
 # プロンプトを2行表示し、現在のモードを表示する。
-# 参考: http://nishikawasasaki.hatenablog.com/entry/20101227/1293459255
+# --------------------------------
 function zle-line-init zle-keymap-select {
   case $KEYMAP in
     vicmd)
@@ -71,17 +73,23 @@ function zle-line-init zle-keymap-select {
 zle -N zle-line-init
 zle -N zle-keymap-select
 
-# ノーマルモードに移行するキーを変更する。
-# 参考: http://qiita.com/syui/items/8cc534c2c30543965950
-bindkey -M viins 'jj' vi-cmd-mode
 
-# 行頭、行末移動
-bindkey -M vicmd '^h' vi-digit-or-beginning-of-line
-bindkey -M vicmd '^l' vi-end-of-line
+# git の情報を表示
+# --------------------------------
+autoload -U vcs_info
+setopt prompt_subst
+zstyle ":vcs_info:*" enable git
+zstyle ":vcs_info:git:*" check-for-changes true
+zstyle ":vcs_info:git:*" stagedstr '%F{yellow}!'
+zstyle ":vcs_info:git:*" unstagedstr '%F{red}+'
+zstyle ":vcs_info:*" formats '%F{green}%c%u[%b:%r]%f'
+zstyle ":vcs_info:*" actionformats '[%b:%r|%a]'
+precmd () { vcs_info }
+RPROMPT='${vcs_info_msg_0_}'
 
-# <Ctrl> + z で vim と shell を切り替える。
-# 本来なら vim (<Ctrl> + z) > shell (fg<Enter>) > vim
-# http://postd.cc/how-to-boost-your-vim-productivity/
+
+# アプリとシェルを切り替える。
+# --------------------------------
 fancy-ctrl-z () {
     if [[ $#BUFFER -eq 0 ]]; then
         BUFFER="fg"
@@ -91,8 +99,22 @@ fancy-ctrl-z () {
         zle clear-screen
     fi
 }
+
 zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
+
+
+# fzfでコマンド実行履歴を表示する。
+# --------------------------------
+function select-history() {
+  BUFFER=$(history -n -r 1 | \
+      fzf-tmux -w '80' -y '16'  \
+      -p --layout=reverse --no-sort +m --query "$LBUFFER" --prompt="History > ")
+  CURSOR=$#BUFFER
+}
+
+zle -N select-history
+bindkey '^f' select-history
 
 # fzfのコマンド履歴から重複を除外する。
 export HISTFILE=~/.zsh_history
@@ -104,18 +126,57 @@ setopt EXTENDED_HISTORY
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_ALL_DUPS
 
-# <C-r>でfzfのコマンド履歴を表示。
-function select-history() {
-  BUFFER=$(history -n -r 1 | \
-      fzf-tmux -w '80' -y '16'  \
-      -p --layout=reverse --no-sort +m --query "$LBUFFER" --prompt="History > ")
-  CURSOR=$#BUFFER
+
+# cdの履歴を保存して、fzfでディレクトリを移動する。
+# --------------------------------
+function run_tac_commnad() {
+  if [[ "$OSTYPE" == "linux"* ]]; then
+    tac ~/.powered_cd.log
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    gtac ~/.powered_cd.log
+  else
+    echo 'Unsupported os.'
+  fi
 }
-zle -N select-history
-bindkey '^f' select-history
 
-# historyに保存するときに余分なスペースを削除する
-setopt hist_reduce_blanks
+function chpwd() {
+  powered_cd_add_log
+}
 
-# ウィンドウ、ペイン間でコマンド履歴を共有する。
-setopt share_history
+function powered_cd_add_log() {
+  local i=0
+  cat ~/.powered_cd.log | while read line; do
+    (( i++ ))
+    if [ i = 30 ]; then
+      sed -i -e "30,30d" ~/.powered_cd.log
+    elif [ "$line" = "$PWD" ]; then
+      sed -i -e "${i},${i}d" ~/.powered_cd.log 
+    fi
+  done
+  echo "$PWD" >> ~/.powered_cd.log
+}
+
+function powered_cd() {
+  if [ $# = 0 ]; then
+    cd $(run_tac_commnad | \
+        fzf-tmux -w '80' -y '16' \
+        -p --layout=reverse --no-sort +m --query "$LBUFFER" --prompt="History > " \
+    )
+    zle reset-prompt
+  elif [ $# = 1 ]; then
+    cd $1
+  else
+    echo "powered_cd: too many arguments"
+  fi
+}
+
+_powered_cd() {
+  _files -/
+}
+
+compdef _powered_cd powered_cd
+
+[ -e ~/.powered_cd.log ] || touch ~/.powered_cd.log
+
+zle -N powered_cd
+bindkey '^r' powered_cd
