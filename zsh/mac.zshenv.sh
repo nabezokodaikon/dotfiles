06f29ce79ca49792cd438f4f778ea33dd1336b11
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-07-07 22:00:03 +0900

    Add Rust setting on zshenv for mac

--- zsh/mac.zshenv.sh
+++ zsh/mac.zshenv.sh
@@ -19,3 +19,8 @@ export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@1.1/include
 export OPENSSL_LIB_DIR=/usr/local/opt/openssl@1.1/lib
 export PKG_CONFIG_PATH=/usr/local/opt/openssl@1.1/lib/pkgconfig
+
+
+# Rust
+#--------------------------------
+. "$HOME/.cargo/env"
