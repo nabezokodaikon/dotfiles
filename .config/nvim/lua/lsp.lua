cf29c799a4b7489ad9f84f5890c19ce7ba32b335
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-07-17 00:17:14 +0900

    Add css lsp setting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -3,7 +3,10 @@
 --------------------------------
 local lsp = require"lspconfig"
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 lsp.cssls.setup {
+  capabilities = capabilities,
 }
 
 lsp.denols.setup{
