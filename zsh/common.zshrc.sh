e595e9946356d1880f3835e088253cc487fba1e8
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-07-20 05:05:34 +0900

    Fix zsh prompt

--- zsh/common.zshrc.sh
+++ zsh/common.zshrc.sh
@@ -57,22 +57,47 @@ setopt share_history
 
 # プロンプトを2行表示し、現在のモードを表示する。
 # --------------------------------
-function zle-line-init zle-line-pre-redraw() {
-  local new="$RPS1"
-  if [[ $KEYMAP != vicmd ]]; then
-    new="INS"
-  elif (( REGION_ACTIVE )); then
-    new="VIS"
-  else
-    new="NOR"
+PROMPT_INS="%{${fg[blue]}%}[%n@%m] %~%{${reset_color}%}
+[INS]$ "
+PROMPT_NOR="%{${fg[blue]}%}[%n@%m] %~%{${reset_color}%}
+[NOR]$ "
+PROMPT_VIS="%{${fg[blue]}%}[%n@%m] %~%{${reset_color}%}
+[VIS]$ "
+
+PROMPT=$PROMPT_INS
+
+function zle-line-pre-redraw {
+  if [[ $REGION_ACTIVE -ne 0 ]]; then
+    NEW_PROMPT=$PROMPT_VIS
+  elif [[ $KEYMAP = vicmd ]]; then
+    NEW_PROMPT=$PROMPT_NOR
+  elif [[ $KEYMAP = main ]]; then
+    NEW_PROMPT=$PROMPT_INS
   fi
-  if [[ $new != $RPS1 ]]; then
-   PROMPT="%{${fg[blue]}%}[%n@%m] %~%{${reset_color}%}
-["$new"]$ "
-   zle reset-prompt
+
+  if [[ $PROMPT = $NEW_PROMPT ]]; then
+    return
   fi
+
+  PROMPT=$NEW_PROMPT
+
+  zle reset-prompt
 }
+
+function zle-keymap-select zle-line-init {
+  case $KEYMAP in
+    vicmd)
+    PROMPT=$PROMPT_NOR
+    ;;
+    main|viins)
+    PROMPT=$PROMPT_INS
+    ;;
+  esac
+  zle reset-prompt
+}
+
 zle -N zle-line-init
+zle -N zle-keymap-select
 zle -N zle-line-pre-redraw
 
 
