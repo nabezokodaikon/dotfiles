e0157bb24ef5672a58507f7877dbef0f1f8041cf
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-07-20 11:16:00 +0900

    Clean zshrc indent

--- zsh/common.zshrc.sh
+++ zsh/common.zshrc.sh
@@ -67,33 +67,34 @@ PROMPT_VIS="%{${fg[blue]}%}[%n@%m] %~%{${reset_color}%}
 PROMPT=$PROMPT_INS
 
 function zle-line-pre-redraw {
-  if [[ $REGION_ACTIVE -ne 0 ]]; then
-    NEW_PROMPT=$PROMPT_VIS
-  elif [[ $KEYMAP = vicmd ]]; then
-    NEW_PROMPT=$PROMPT_NOR
-  elif [[ $KEYMAP = main ]]; then
-    NEW_PROMPT=$PROMPT_INS
-  fi
+    if [[ $REGION_ACTIVE -ne 0 ]]; then
+        NEW_PROMPT=$PROMPT_VIS
+    elif [[ $KEYMAP = vicmd ]]; then
+        NEW_PROMPT=$PROMPT_NOR
+    elif [[ $KEYMAP = main ]]; then
+        NEW_PROMPT=$PROMPT_INS
+    fi
 
-  if [[ $PROMPT = $NEW_PROMPT ]]; then
-    return
-  fi
+    if [[ $PROMPT = $NEW_PROMPT ]]; then
+        return
+    fi
 
-  PROMPT=$NEW_PROMPT
+    PROMPT=$NEW_PROMPT
 
-  zle reset-prompt
+    zle reset-prompt
 }
 
 function zle-keymap-select zle-line-init {
-  case $KEYMAP in
-    vicmd)
-    PROMPT=$PROMPT_NOR
-    ;;
-    main|viins)
-    PROMPT=$PROMPT_INS
-    ;;
-  esac
-  zle reset-prompt
+    case $KEYMAP in
+        vicmd)
+            PROMPT=$PROMPT_NOR
+            ;;
+        main|viins)
+            PROMPT=$PROMPT_INS
+            ;;
+    esac
+
+    zle reset-prompt
 }
 
 zle -N zle-line-init
