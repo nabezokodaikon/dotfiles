b9c0fac5594a96aa465be2facd7136478481026d
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-10-15 00:18:03 +0900

    Modify nvim-tree settings

--- .config/nvim/lua/tree.lua
+++ .config/nvim/lua/tree.lua
@@ -1,21 +1,24 @@
-local g = vim.g
-
-g.nvim_tree_quit_on_open = 1
-g.nvim_tree_width = 40
-
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-g.nvim_tree_bindings = {
-  { key = {"<CR>", "o"}, cb = tree_cb("edit") },
-  { key = {"~"},         cb = tree_cb("cd") },
-  { key = "<Tab>",       cb = tree_cb("preview") },
-  { key = "R",           cb = tree_cb("refresh") },
-  { key = "<C-n>",       cb = tree_cb("create") },
-  { key = "D",           cb = tree_cb("remove") },
-  { key = "r",           cb = tree_cb("rename") },
-  { key = "m",           cb = tree_cb("cut") },
-  { key = "c",           cb = tree_cb("copy") },
-  { key = "p",           cb = tree_cb("paste") },
-  { key = "y",           cb = tree_cb("copy_name") },
-  { key = "Y",           cb = tree_cb("copy_absolute_path") },
-  { key = "q",           cb = tree_cb("close") },
+
+require'nvim-tree'.setup {
+  view = {
+    width = 35,
+    mappings = {
+      list = {
+        { key = {"<CR>", "o"}, cb = tree_cb("edit") },
+        { key = {"~"},         cb = tree_cb("cd") },
+        { key = "<Tab>",       cb = tree_cb("preview") },
+        { key = "R",           cb = tree_cb("refresh") },
+        { key = "<C-n>",       cb = tree_cb("create") },
+        { key = "D",           cb = tree_cb("remove") },
+        { key = "r",           cb = tree_cb("rename") },
+        { key = "m",           cb = tree_cb("cut") },
+        { key = "c",           cb = tree_cb("copy") },
+        { key = "p",           cb = tree_cb("paste") },
+        { key = "y",           cb = tree_cb("copy_name") },
+        { key = "Y",           cb = tree_cb("copy_absolute_path") },
+        { key = "q",           cb = tree_cb("close") },
+      },
+    },
+  },
 }
