8b1cb285f5a2d721883d05014a1d22a325422684
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-11-07 01:28:44 +0900

    Modify filename to filepath on status line

--- .config/nvim/lua/status-line.lua
+++ .config/nvim/lua/status-line.lua
@@ -11,7 +11,12 @@ lualine.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        path = 2,
+      }
+    },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'},
