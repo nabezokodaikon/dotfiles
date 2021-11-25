77ec37ec2e2de5846974005a1285193a1743e0bc
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-11-26 00:57:46 +0900

    Modify OpenSSL PATH

--- zsh/mac.zshenv.sh
+++ zsh/mac.zshenv.sh
@@ -10,6 +10,6 @@ export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 
 # OpenSSL
 #--------------------------------
-export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@1.1/include
-export OPENSSL_LIB_DIR=/usr/local/opt/openssl@1.1/lib
-export PKG_CONFIG_PATH=/usr/local/opt/openssl@1.1/lib/pkgconfig
+export OPENSSL_INCLUDE_DIR=/opt/homebrew/opt/openssl@1.1/include
+export OPENSSL_LIB_DIR=/opt/homebrew/opt/openssl@1.1/lib
+export PKG_CONFIG_PATH=/opt/homebrew/opt/openssl@1.1/lib/pkgconfig
