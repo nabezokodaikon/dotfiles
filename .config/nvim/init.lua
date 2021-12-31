7d725bda4e3e14c9eddd9e545959f9e3ea70210f
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-01-01 04:30:38 +0900

    Modify init file of Neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,37 @@
+--------------------------------
+-- Initial settings
+--------------------------------
+vim.cmd('filetype off')
+vim.cmd('syntax off')
+
+
+-- TODO: OS判定。
+-- https://github.com/glepnir/nvim/blob/master/lua/core/global.lua
+
+
+-- Set disable providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+
+-- Basic settings
+require('basic')
+
+-- Key mappings
+require('mappings')
+
+-- File type settings
+require('filetypes')
+
+-- Tab settings
+vim.cmd [[source ~/.config/nvim/rc/tab.rc.vim]]
+
+-- プラグイン管理設定
+vim.cmd [[source ~/.config/nvim/rc/dein/dein.rc.vim]]
+
+
+vim.cmd('filetype plugin indent on')
+vim.cmd('syntax on')
