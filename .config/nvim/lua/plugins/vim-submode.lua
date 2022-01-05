47fa33b176254d06bdbeefb23e95e479ed4581a9
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-01-05 13:47:55 +0900

    Modify vimscript to lua for vim-submode

--- .config/nvim/lua/plugins/vim-submode.lua
+++ .config/nvim/lua/plugins/vim-submode.lua
@@ -1,17 +1,15 @@
-vim.cmd [[
-  " Window resize.
-  call submode#enter_with('window-resize', 'n', '', '<C-w>>', '<C-w>>')
-  call submode#enter_with('window-resize', 'n', '', '<C-w><', '<C-w><')
-  call submode#enter_with('window-resize', 'n', '', '<C-w>+', '<C-w>+')
-  call submode#enter_with('window-resize', 'n', '', '<C-w>-', '<C-w>-')
-  call submode#map('window-resize', 'n', '', '>', '<C-w>>')
-  call submode#map('window-resize', 'n', '', '<', '<C-w><')
-  call submode#map('window-resize', 'n', '', '+', '<C-w>+')
-  call submode#map('window-resize', 'n', '', '-', '<C-w>-')
+-- Window resize.
+vim.call('submode#enter_with', 'window-resize', 'n', '', '<C-w>>', '<C-w>>')
+vim.call('submode#enter_with', 'window-resize', 'n', '', '<C-w><', '<C-w><')
+vim.call('submode#enter_with', 'window-resize', 'n', '', '<C-w>+', '<C-w>+')
+vim.call('submode#enter_with', 'window-resize', 'n', '', '<C-w>-', '<C-w>-')
+vim.call('submode#map', 'window-resize', 'n', '', '>', '<C-w>>')
+vim.call('submode#map', 'window-resize', 'n', '', '<', '<C-w><')
+vim.call('submode#map', 'window-resize', 'n', '', '+', '<C-w>+')
+vim.call('submode#map', 'window-resize', 'n', '', '-', '<C-w>-')
 
-  " Tab change.
-  call submode#enter_with('tab-next', 'n', '', 'gt', 'gt')
-  call submode#map('tab-next', 'n', '', 't', 'gt')
-  call submode#enter_with('tab-preview', 'n', '', 'gT', 'gT')
-  call submode#map('tab-preview', 'n', '', 'T', 'gT')
-]]
+-- Tab change.
+vim.call('submode#enter_with', 'tab-next', 'n', '', 'gt', 'gt')
+vim.call('submode#map', 'tab-next', 'n', '', 't', 'gt')
+vim.call('submode#enter_with', 'tab-preview', 'n', '', 'gT', 'gT')
+vim.call('submode#map', 'tab-preview', 'n', '', 'T', 'gT')
