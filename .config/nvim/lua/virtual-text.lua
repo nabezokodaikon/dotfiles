3682cbb8b1c1f65c92e8f7af0c977df94d02ba7b
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-01-05 14:05:18 +0900

    Modify virtualtext settings

--- .config/nvim/lua/virtual-text.lua
+++ .config/nvim/lua/virtual-text.lua
@@ -1,16 +1,11 @@
 --------------------------------
--- Virtual text configurations
+-- Virtual text settings
 --------------------------------
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-        underline = false,
+        underline = true,
         virtual_text = true,
         signs = true,
         update_in_insert = true,
     }
 )
-
-vim.fn.sign_define('LspDiagnosticsSignError', { text = "", texthl = "LspDiagnosticsDefaultError" })
-vim.fn.sign_define('LspDiagnosticsSignWarning', { text = "", texthl = "LspDiagnosticsDefaultWarning" })
-vim.fn.sign_define('LspDiagnosticsSignInformation', { text = "", texthl = "LspDiagnosticsDefaultInformation" })
-vim.fn.sign_define('LspDiagnosticsSignHint', { text = "", texthl = "LspDiagnosticsDefaultHint" })
