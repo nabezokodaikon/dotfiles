1019a4e002a9330e419d3f8726f6d55dc81c9b38
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-01-09 16:23:49 +0900

    Modify windws setup batch

--- script/install-windows.bat
+++ script/install-windows.bat
@@ -5,7 +5,7 @@ del %USERPROFILE%\.tigrc
 
 REM Install
 mkdir %LOCALAPPDATA%\nvim
-mklink %LOCALAPPDATA%\nvim\init.vim %USERPROFILE%\dotfiles\.config\nvim\init.vim
+mklink %LOCALAPPDATA%\nvim\init.lua %USERPROFILE%\dotfiles\.config\nvim\init.lua
 mklink %LOCALAPPDATA%\nvim\ginit.vim %USERPROFILE%\dotfiles\.config\nvim\ginit.vim
 mklink /D %USERPROFILE%\.config %USERPROFILE%\dotfiles\.config
 mklink %USERPROFILE%\.tigrc %USERPROFILE%\dotfiles\tig\.tigrc
