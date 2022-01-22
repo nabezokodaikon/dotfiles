55ae4c9edae34c729a0178076adf8ce06a51e9e0
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-01-23 04:20:43 +0900

    Remove brew alias

--- zsh/mac.zshrc.sh
+++ zsh/mac.zshrc.sh
@@ -18,7 +18,6 @@ alias ls="ls -G"
 alias vim="env LANG=en_US.UTF-8 vim \"\$@\""
 alias nvim="env LANG=en_US.UTF-8 nvim \"\$@\""
 alias tmux="env LANG=en_US.UTF-8 tmux -2u \"\$@\""
-alias brew="PATH=/opt/homebrew/bin brew"
 
 
 # nvm lazy loading
