f4212c35bb9e0366a2808307482b3e6c2b63f3c9
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-02-04 02:00:45 +0900

    Modify file search source on ddu.vim

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -43,7 +43,7 @@ vim.keymap.set('n', '<Leader>e', '<cmd>NvimTreeToggle<CR>', { noremap = true, si
 vim.keymap.set('v', '<Leader>t', '<cmd>TranslateJa2En<CR>', { noremap = true, silent = true })
 vim.keymap.set('v', '<Leader>j', '<cmd>TranslateEn2Ja<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>m', '<Plug>(easymotion-overwin-f2)', { noremap = false }) 
-vim.keymap.set('n', '<Leader>f', '<cmd>Ddu file_rec<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>f', '<cmd>Ddu file_rg<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>h', '<cmd>Ddu mr -source-param-current=v:true<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>b', '<cmd>Ddu buffer<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>g', '<cmd>DduRg<CR>', { noremap = true, silent = true })
