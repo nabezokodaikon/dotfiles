b47a56c76184abf8c5cc95787a9e0627b1b004cc
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-02-05 19:25:46 +0900

    Fix reference PATH on brew

--- zsh/mac.zshrc.sh
+++ zsh/mac.zshrc.sh
@@ -4,9 +4,6 @@
 
 # PATH
 #--------------------------------
-export PATH=/usr/local/bin:$PATH
-export PATH=/opt/homebrew/bin:$PATH
-export PATH=/opt/homebrew/opt/openjdk/bin:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.deno/bin:$PATH
@@ -18,7 +15,7 @@ alias ls="ls -G"
 alias vim="env LANG=en_US.UTF-8 vim \"\$@\""
 alias nvim="env LANG=en_US.UTF-8 nvim \"\$@\""
 alias tmux="env LANG=en_US.UTF-8 tmux -2u \"\$@\""
-
+alias brew="PATH=/opt/homebrew/bin brew"
 
 # nvm lazy loading
 #--------------------------------
