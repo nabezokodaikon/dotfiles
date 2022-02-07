6d28f848bb470721c918a1d129d84d78bb1003da
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-02-07 21:23:30 +0900

    Modify rename ddu-ui-std to ddu-ui-ff

--- .config/nvim/lua/plugins/ddu.lua
+++ .config/nvim/lua/plugins/ddu.lua
@@ -1,5 +1,5 @@
 vim.call('ddu#custom#patch_global',
-  'ui', {'std'})
+  'ui', {'ff'})
 vim.call('ddu#custom#patch_global',
   'sourceOptions', {_ = {matchers = {'matcher_fzf'}}})
 vim.call('ddu#custom#patch_global',
@@ -19,7 +19,7 @@ vim.call('ddu#custom#patch_global',
 local winWidth = math.floor(vim.o.columns * 0.8)
 local winCol = math.floor((vim.o.columns - winWidth) / 2)
 vim.call('ddu#custom#patch_global',
-  'uiParams', {std = {
+  'uiParams', {ff = {
     filterFloatingPosition = 'top',
     prompt = '>',
     split ='floating',
@@ -31,9 +31,9 @@ vim.call('ddu#custom#patch_global',
 local opt = { noremap = true, buffer = true, silent = true}
 
 function ddu_my_settings()
-  vim.keymap.set('n', '<CR>', "<Cmd>call ddu#ui#std#do_action('itemAction')<CR>", opt)
-  vim.keymap.set('n', 'i', "<Cmd>call ddu#ui#std#do_action('openFilterWindow')<CR>", opt)
-  vim.keymap.set('n', 'q', "<Cmd>call ddu#ui#std#do_action('quit')<CR>", opt)
+  vim.keymap.set('n', '<CR>', "<Cmd>call ddu#ui#ff#do_action('itemAction')<CR>", opt)
+  vim.keymap.set('n', 'i', "<Cmd>call ddu#ui#ff#do_action('openFilterWindow')<CR>", opt)
+  vim.keymap.set('n', 'q', "<Cmd>call ddu#ui#ff#do_action('quit')<CR>", opt)
 end
 
 function ddu_filter_my_settings()
@@ -43,6 +43,6 @@ function ddu_filter_my_settings()
 end
 
 vim.cmd[[
-autocmd FileType ddu-std lua ddu_my_settings()
-autocmd FileType ddu-std-filter lua ddu_filter_my_settings()
+autocmd FileType ddu-ff lua ddu_my_settings()
+autocmd FileType ddu-ff-filter lua ddu_filter_my_settings()
 ]]
