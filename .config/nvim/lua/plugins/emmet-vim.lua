3184489cc055b6d2df53e74b27168b1feaab0f81
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-03-01 23:12:24 +0900

    Clean emmet-vim setting

--- .config/nvim/lua/plugins/emmet-vim.lua
+++ .config/nvim/lua/plugins/emmet-vim.lua
@@ -2,7 +2,15 @@ vim.g.user_emmet_install_global = false
 vim.g.user_emmet_leader_key = '<C-e>'
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'html,css,scss,javascript,javascriptreact,typescript,typescriptreact',
+  pattern = {
+    'html',
+    'css',
+    'scss',
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact'
+  },
   callback = function()
                vim.cmd('EmmetInstall')
              end,
