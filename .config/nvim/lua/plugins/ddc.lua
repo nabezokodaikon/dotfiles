389c8d74d231699e582346d7f2e5514c71f1a4d0
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-04-24 19:48:23 +0900

    Add ddc-file plugin

--- .config/nvim/lua/plugins/ddc.lua
+++ .config/nvim/lua/plugins/ddc.lua
@@ -1,5 +1,5 @@
 vim.call('ddc#custom#patch_global',
-  'sources', {'nvim-lsp', 'around'})
+  'sources', {'nvim-lsp', 'file', 'around'})
 
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {_ = {matchers = {'matcher_head'}}})
@@ -9,6 +9,12 @@ vim.call('ddc#custom#patch_global',
   'sourceOptions', {['around'] = {mark = {'Buf'}}})
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['nvim-lsp'] = {mark = {'LSP'}}})
+vim.call('ddc#custom#patch_global',
+  'sourceOptions', {['file'] = {mark = {'File'}}})
+vim.call('ddc#custom#patch_global',
+  'sourceOptions', {['file'] = {isVolatile = {true}}})
+vim.call('ddc#custom#patch_global',
+  'sourceOptions', {['file'] = {forceCompletionPattern = {[[\S/\S*]]}}})
 
 vim.call('ddc#custom#patch_filetype', 'rust',
   'sourceOptions', {['nvim-lsp'] = {forceCompletionPattern = {[[\.\w*|::\w*]]}}})
