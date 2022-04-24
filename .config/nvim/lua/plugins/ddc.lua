99f5511aa7a12be3c8ea63b109b3e6b22942f972
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-04-24 19:49:04 +0900

    Clean ddc.vim setting

--- .config/nvim/lua/plugins/ddc.lua
+++ .config/nvim/lua/plugins/ddc.lua
@@ -5,10 +5,13 @@ vim.call('ddc#custom#patch_global',
   'sourceOptions', {_ = {matchers = {'matcher_head'}}})
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {_ = {sorters = {'sorter_rank'}}})
+
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['around'] = {mark = {'Buf'}}})
+
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['nvim-lsp'] = {mark = {'LSP'}}})
+
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['file'] = {mark = {'File'}}})
 vim.call('ddc#custom#patch_global',
