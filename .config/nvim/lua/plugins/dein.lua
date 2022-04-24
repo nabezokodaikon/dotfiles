267f287d40bf41c904c0eb756264817bfadb5bd2
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-04-24 19:26:13 +0900

    Modify dein.vim setting

--- .config/nvim/lua/plugins/dein.lua
+++ .config/nvim/lua/plugins/dein.lua
@@ -21,7 +21,7 @@ local dein_toml = dein_toml_dir .. '/dein.toml'
 local dein_toml_lazy = dein_toml_dir .. '/deinlazy.toml'
 local dein_toml_nvim_cmp = dein_toml_dir .. '/nvim-cmp.toml'
 
-vim.call('dein#begin', dein_dir, { vim.fn.expand('<sfile>'), dein_toml, dein_toml_lazy })
+vim.call('dein#begin', dein_dir)
 
 vim.call('dein#load_toml', dein_toml, { lazy = 0 })
 vim.call('dein#load_toml', dein_toml_lazy, { lazy = 1 })
