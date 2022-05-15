c9de171fa31d1941bedccfcda75f3394ff9d4974
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-05-15 23:55:13 +0900

    Modify plugins to lazy

--- .config/nvim/lua/plugins/ddc.lua
+++ .config/nvim/lua/plugins/ddc.lua
@@ -1,18 +1,18 @@
--- vim.call('ddc#custom#patch_global',
-  -- 'sources', {'nvim-lsp', 'file', 'around'})
 vim.call('ddc#custom#patch_global',
-  'sources', {'nvim-lsp'})
+  'sources', {'nvim-lsp_by-treesitter', 'file', 'around'})
 
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {_ = {matchers = {'matcher_head'}}})
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {_ = {sorters = {'sorter_rank'}}})
+vim.call('ddc#custom#patch_global',
+  'sourceOptions', {_ = {minAutoCompleteLength = {1}}})
 
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['around'] = {mark = {'Buf'}}})
 
 vim.call('ddc#custom#patch_global',
-  'sourceOptions', {['nvim-lsp'] = {mark = {'LSP'}}})
+  'sourceOptions', {['nvim-lsp_by-treesitter'] = {mark = {'LSP'}}})
 
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['file'] = {mark = {'File'}}})
@@ -20,8 +20,5 @@ vim.call('ddc#custom#patch_global',
   'sourceOptions', {['file'] = {isVolatile = {true}}})
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['file'] = {forceCompletionPattern = {[[\S/\S*]]}}})
-
--- vim.call('ddc#custom#patch_filetype', 'rust',
-  -- 'sourceOptions', {['nvim-lsp'] = {forceCompletionPattern = {[[\.\w*|::\w*]]}}})
 
 vim.call('ddc#enable')
