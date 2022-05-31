704fa4ef591d0543d9d56dd94e2304ab4a0c3919
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-05-31 16:32:54 +0900

    Fix ddc.vim settings

--- .config/nvim/lua/plugins/ddc.lua
+++ .config/nvim/lua/plugins/ddc.lua
@@ -6,22 +6,22 @@ vim.call('ddc#custom#patch_global',
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {_ = {sorters = {'sorter_rank'}}})
 vim.call('ddc#custom#patch_global',
-  'sourceOptions', {_ = {minAutoCompleteLength = {1}}})
+  'sourceOptions', {_ = {minAutoCompleteLength = 1}})
 
 vim.call('ddc#custom#patch_global',
-  'sourceOptions', {['around'] = {mark = {'[Buf]'}}})
+  'sourceOptions', {['around'] = {mark = '[Buf]'}})
 
 vim.call('ddc#custom#patch_global',
-  'sourceOptions', {['nvim-lsp'] = {mark = {'[LSP]'}}})
+  'sourceOptions', {['nvim-lsp'] = {mark = '[LSP]'}})
 
 vim.call('ddc#custom#patch_global',
-  'sourceOptions', {['file'] = {mark = {'[File]'}}})
+  'sourceOptions', {['file'] = {mark = '[File]'}})
 vim.call('ddc#custom#patch_global',
   'sourceOptions', {['file'] = {isVolatile = {true}}})
 vim.call('ddc#custom#patch_global',
-  'sourceOptions', {['file'] = {forceCompletionPattern = {[[\S/\S*]]}}})
+  'sourceOptions', {['file'] = {forceCompletionPattern = [[\S/\S*]]}})
 
 vim.call('ddc#custom#patch_filetype', 'rust',
-  'sourceOptions', {['nvim-lsp'] = {forceCompletionPattern = {[[\.\w*|::\w*]]}}})
+  'sourceOptions', {['nvim-lsp'] = {forceCompletionPattern = [[\.\w*|::\w*]]}})
 
 vim.call('ddc#enable')
