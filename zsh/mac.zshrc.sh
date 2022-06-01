9e20494b3085f0de4731c73828539f2ebc6694bd
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-06-02 03:18:56 +0900

    Remove lua LSP server PATH

--- zsh/mac.zshrc.sh
+++ zsh/mac.zshrc.sh
@@ -8,7 +8,6 @@ export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.deno/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
-export PATH=$HOME/ws/src/lua-language-server/bin:$PATH
 
 # Alias
 #--------------------------------
