98f3347704b55c01b49ba954b12e949b7ed5eb86
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-06-02 07:35:33 +0900

    Fix alias

--- zsh/alias/common.alias.sh
+++ zsh/alias/common.alias.sh
@@ -3,8 +3,8 @@
 #--------------------------------
 alias ls="ls -F --color=auto"
 alias ll="ls -ahl"
-alias lls="lsn -rS"
-alias llt="lsn -rt"
+alias lls="ls -rS"
+alias llt="ls -rt"
 
 alias nvim="env LANG=en_US.UTF-8 nvim \"\$@\""
 alias tmux="env LANG=en_US.UTF-8 tmux -2u \"\$@\""
