a9042ae385388401d9aaaab5daed5b966c29321e
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-06-30 05:37:11 +0900

    Modify init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,35 +1,40 @@
---------------------------------
--- Initial settings
---------------------------------
 vim.cmd('syntax off')
 vim.cmd('filetype off')
 vim.cmd('filetype plugin indent off')
 
+local cache_dir = vim.env.HOME .. '/.cache'
+local dein_dir = cache_dir .. '/dein'
+local dein_repo_dir = dein_dir .. '/repos/github.com/Shougo/dein.vim'
 
--- TODO: OS判定。
--- https://github.com/glepnir/nvim/blob/master/lua/core/global.lua
+if not string.match(vim.o.runtimepath, '/dein.vim') then
+	if vim.fn.isdirectory(dein_repo_dir) ~= 1 then
+		os.execute('git clone https://github.com/Shougo/dein.vim ' .. dein_repo_dir)
+	end
+	vim.o.runtimepath = dein_repo_dir .. ',' .. vim.o.runtimepath 
+end
+
+if vim.call('dein#min#load_state', dein_dir) ~= 1 then
+    return
+end
+
+vim.call('dein#begin', dein_dir)
+
+vim.call('dein#add', 'neovim/nvim-lspconfig')
+vim.call('dein#add', 'hrsh7th/nvim-cmp')
+vim.call('dein#add', 'hrsh7th/cmp-nvim-lsp')
+
+vim.call('dein#end')
+vim.call('dein#save_state')
 
 
--- Set disable providers
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_python_provider = 0
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
+require'lspconfig'.rust_analyzer.setup {}
 
-
--- Basic settings.
-require('basic')
-
--- Key mappings.
-require('keymap')
-
--- Virtual text settings.
-require('virtual-text')
-
--- Plugins settings.
-require('plugins.dein')
-
+local cmp = require('cmp')
+cmp.setup {
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }),
+}
 
 vim.cmd('syntax on')
 vim.cmd('filetype on')
