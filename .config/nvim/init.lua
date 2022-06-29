b8e7fc907273a98797f83e2b64a5e457cd797a7a
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-06-30 06:02:06 +0900

    Add rust plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,6 +19,7 @@ end
 
 vim.call('dein#begin', dein_dir)
 
+vim.call('dein#add', 'rust-lang/rust.vim')
 vim.call('dein#add', 'neovim/nvim-lspconfig')
 vim.call('dein#add', 'hrsh7th/nvim-cmp')
 vim.call('dein#add', 'hrsh7th/cmp-nvim-lsp')
@@ -30,7 +31,18 @@ vim.call('dein#save_state')
 require'lspconfig'.rust_analyzer.setup {}
 
 local cmp = require('cmp')
+
 cmp.setup {
+
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      select = true,
+    }),
+  },
+
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
   }),
