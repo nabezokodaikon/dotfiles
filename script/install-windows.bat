eed155c9212a8254f5cb898b0f7b6664cdbd9231
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-23 14:57:06 +0900

    Add VSCode setting.json

--- script/install-windows.bat
+++ script/install-windows.bat
@@ -9,3 +9,4 @@ mklink %LOCALAPPDATA%\nvim\init.lua %USERPROFILE%\dotfiles\.config\nvim\init.lua
 mklink %LOCALAPPDATA%\nvim\ginit.vim %USERPROFILE%\dotfiles\.config\nvim\ginit.vim
 mklink /D %USERPROFILE%\.config %USERPROFILE%\dotfiles\.config
 mklink %USERPROFILE%\.tigrc %USERPROFILE%\dotfiles\tig\.tigrc
+mklink %APPDATA%\Code\User\settings.json %USERPROFILE%\dotfiles\vscode\settings.json
