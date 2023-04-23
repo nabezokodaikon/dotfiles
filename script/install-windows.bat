4d262dd5ce0330fb6d210e4d9e71523a88b51afe
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-23 15:16:08 +0900

    Add keybindings.json on VSCode

--- script/install-windows.bat
+++ script/install-windows.bat
@@ -2,6 +2,8 @@ REM Uninstall
 rmdir /s /q %LOCALAPPDATA%\nvim
 rmdir %USERPROFILE%\.config
 del %USERPROFILE%\.tigrc
+del %AppData%\Code\User\settings.json
+del %AppData%\Code\User\keybindings.json
 
 REM Install
 mkdir %LOCALAPPDATA%\nvim
@@ -10,3 +12,4 @@ mklink %LOCALAPPDATA%\nvim\ginit.vim %USERPROFILE%\dotfiles\.config\nvim\ginit.v
 mklink /D %USERPROFILE%\.config %USERPROFILE%\dotfiles\.config
 mklink %USERPROFILE%\.tigrc %USERPROFILE%\dotfiles\tig\.tigrc
 mklink %APPDATA%\Code\User\settings.json %USERPROFILE%\dotfiles\vscode\settings.json
+mklink %APPDATA%\Code\User\keybindings.json %USERPROFILE%\dotfiles\vscode\keybindings.json
