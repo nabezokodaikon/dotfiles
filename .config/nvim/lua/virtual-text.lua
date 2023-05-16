328558d5aa4a1ae1fae83cdcd30d03cc494171eb
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-05-16 19:09:30 +0900

    Modify virtual-text display method

--- .config/nvim/lua/virtual-text.lua
+++ .config/nvim/lua/virtual-text.lua
@@ -4,8 +4,13 @@
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         underline = true,
-        virtual_text = true,
+        virtual_text = false,
         signs = true,
         update_in_insert = true,
     }
 )
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
