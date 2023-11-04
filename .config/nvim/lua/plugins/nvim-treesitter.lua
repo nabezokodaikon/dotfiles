d390292076d7be437699434d10a42a582a1ae9a4
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-11-04 20:30:34 +0900

    Fix treesitter setting

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,26 +4,35 @@
 local treesitter = require"nvim-treesitter.configs"
 
 treesitter.setup {
-    ensure_installed = {
-      "bash",
-      "c",
-      "css",
-      "dockerfile",
-      "graphql",
-      "help",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "rust",
-      "toml",
-      "typescript",
-      "vim",
-      "yaml",
-    },
-    sync_install = false,
-    highlight = {
-        enable = true,
-    }
+  ensure_installed = {
+    "bash",
+    "css",
+    "dockerfile",
+    "graphql",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "rust",
+    "svelte",
+    "toml",
+    "typescript",
+    "vim",
+    "yaml",
+  },
+  sync_install = false,
+  highlight = {
+      enable = true,
+  },
+  context_commentstring = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
 }
+
+require('Comment').setup({
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+})
