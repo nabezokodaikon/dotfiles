d4194b78dff6c20f3569e97e30eab84dfee45609
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-11-23 18:41:33 +0900

    Modify nvim-cmp setting

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,11 @@
 local lsp = require"lspconfig"
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 -- lsp.bashls.setup{
 -- }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 lsp.cssls.setup {
   capabilities = capabilities,
 }
